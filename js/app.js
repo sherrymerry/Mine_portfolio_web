d6dc8f19957dd9a57d5e59d2850f802280ee172f
Shahryar-waseem <sherrymerry20@gmail.com>
2024-03-29 23:43:23 +0500

    counter updated

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
   
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        var counterElement = document.querySelector('.counter, .number1, .number2, .number3, .number4');
-        var sectionOffsetTop = counterElement.getBoundingClientRect().top;
-        var windowHeight = window.innerHeight;
-        
-        // Check if the section is in view
-        if (sectionOffsetTop < windowHeight) {
-            countdown('.counter', 0, 120);
-            countdown('.number1', 0, 100);
-            countdown('.number2', 0, 1200);
-            countdown('.number3', 0, 1200);
-            countdown('.number4', 1, 500);
-        }
-    });
-});
 
+    document.addEventListener('DOMContentLoaded', function() {
+
+        countdown('.counter', 0, 120);
+        countdown('.number1', 0, 100);
+        countdown('.number2', 0, 1200);
+        countdown('.number3', 0, 1200);
+  
+    });
+    
+    function countdown(className, start, end) {
+        let currentValue = start;
+        let increment = (end - start) / 40; // Adjust speed by changing the divisor (40 here)
+        let interval = setInterval(function() {
+            currentValue += increment;
+            document.querySelector(className).innerText = Math.round(currentValue).toLocaleString();
+            if (currentValue >= end) {
+                clearInterval(interval);
+            }
+        }, 60); // Adjust speed by changing the interval (50ms here)
+    }
+
+    
+    
 function countdown(className, start, end) {
     let currentValue = start;
     let increment = (end - start) / 40; // Adjust speed by changing the divisor (40 here)
@@ -45,3 +52,6 @@ window.addEventListener('load', function () {
     preloader.style.display = 'none'; // Hide the preloader when the page is fully loaded
     document.body.style.overflow = 'auto'; // Enable scrolling after the page is loaded
 });
+
+
+
