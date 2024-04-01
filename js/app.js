3942345dbef7edff6d6633d177c769c08282808c
Shahryar-waseem <sherrymerry20@gmail.com>
2024-04-02 03:40:33 +0500

    responsiveness edited

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
     document.addEventListener('DOMContentLoaded', function() {
 
-        countdown('.counter', 1, 120);
+        countdown('.counter', 0, 120);
         countdown('.number1', 0, 100);
         countdown('.number2', 0, 1200);
         countdown('.number3', 0, 1200);
@@ -22,19 +22,7 @@
         }, 60); // Adjust speed by changing the interval (50ms here)
     }
 
-    
-    
-function countdown(className, start, end) {
-    let currentValue = start;
-    let increment = (end - start) / 40; // Adjust speed by changing the divisor (40 here)
-    let interval = setInterval(function() {
-        currentValue += increment;
-        document.querySelector(className).innerText = Math.round(currentValue).toLocaleString();
-        if (currentValue >= end) {
-            clearInterval(interval);
-        }
-    }, 70); // Adjust speed by changing the interval (50ms here)
-}
+
 
 
 
